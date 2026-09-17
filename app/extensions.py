0a8236a7d9e2a9b722adf8370e8875c0b818e010
sydomori <sydneyomori@gmail.com>
2026-09-17 15:22:21 +0300

    feat: add mail instance to extension.py

--- app/extensions.py
+++ app/extensions.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask_mail import Mail
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
 cors = CORS()
+mail = Mail()
