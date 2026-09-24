f52c278f30e739bdf0532b988aef2f8690be28cf
sydomori <sydneyomori@gmail.com>
2026-09-24 20:11:32 +0300

    feat: add invalid_type_is_rejected test

--- tests/communications/test_schemas.py
+++ tests/communications/test_schemas.py
@@ -6,3 +6,9 @@ def test_valid_payload_loads_cleanly():
     schema = TruckIssueCreateSchema()
     data = schema.load({"truck_id": 1, "type": "fuel", "description": "Low fuel"})
     assert data == {"truck_id": 1, "type": "fuel", "description": "Low fuel"}
+
+def test_invalid_type_is_rejected():
+    schema = TruckIssueCreateSchema()
+    with pytest.raises(ValidationError) as exc_info:
+        schema.load({"truck_id": 1, "type": "explosion", "description": "..."})
+    assert "type" in exc_info.value.messages
